de3edde3d7a5b970592a8eb252479c14e7ef5209
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-02-17 07:34:54 +0100

    chpre: does this work?

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    silent: false,
     alias: {
       "#supports": new URL("./src/supports.ts", import.meta.url).pathname,
     },
