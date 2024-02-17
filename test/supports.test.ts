fa1302720973b64f4f516c5ce2cd7306c3980602
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-02-17 07:08:17 +0100

    test: import `isColorsSupported` inside test fn

--- test/supports.test.ts
+++ test/supports.test.ts
@@ -1,7 +1,6 @@
 import process from "node:process";
 import tty from "node:tty";
 import { beforeEach, expect, it, vi } from "vitest";
-import { isColorsSupported } from "../src/supports";
 
 beforeEach(() => {
   Object.defineProperty(process, "platform", {
@@ -11,33 +10,43 @@ beforeEach(() => {
   process.argv = [];
 });
 
-it("should return false if NO_COLOR is in env", () => {
+it("should return false if NO_COLOR is in env", async () => {
   vi.stubEnv("NO_COLOR", "1");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(false);
 });
 
-it("should return false if --no-color is in argv", () => {
+it("should return false if --no-color is in argv", async () => {
   process.argv.push("--no-color");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(false);
 });
 
-it("should return true if FORCE_COLOR is in env", () => {
+it("should return true if FORCE_COLOR is in env", async () => {
   vi.stubEnv("FORCE_COLOR", "1");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(true);
 });
 
-it("should return true if --color is in argv", () => {
+it("should return true if --color is in argv", async () => {
   process.argv.push("--color");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(true);
 });
 
-it("should return false if not TTY", () => {
+it("should return false if not TTY", async () => {
   tty.isatty = vi.fn(() => false);
+  const { isColorsSupported } = await import("#supports");
   expect(isColorsSupported()).toBe(false);
 });
 
-it("return false if `CI` is in env", () => {
+it("return false if `CI` is in env", async () => {
   vi.stubEnv("CI", "1");
+  const { isColorsSupported } = await import("#supports");
   expect(isColorsSupported()).toBe(false);
 });
 
@@ -53,41 +62,52 @@ const CIS = [
 ];
 
 for (const ci of CIS) {
-  it(`should return true when \`CI\` is set and \`${ci}\` is set`, () => {
+  it(`should return true when \`CI\` is set and \`${ci}\` is set`, async () => {
     vi.stubEnv("CI", "1");
     vi.stubEnv(ci, "1");
+    const { isColorsSupported } = await import("#supports");
+
     expect(isColorsSupported()).toBe(true);
   });
 }
 
-it("should return true if platform is win32 and TERM is not dumb", () => {
+it("should return true if platform is win32 and TERM is not dumb", async () => {
   vi.stubEnv("TERM", "xterm");
   Object.defineProperty(process, "platform", { value: "win32" });
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(true);
 });
 
-it("should return false if platform is win32 and TERM is dumb", () => {
+it("should return false if platform is win32 and TERM is dumb", async () => {
   vi.stubEnv("TERM", "dumb");
   Object.defineProperty(process, "platform", { value: "win32" });
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(false);
 });
 
-it("return false when `TERM` is set to dumb", () => {
+it("return false when `TERM` is set to dumb", async () => {
   vi.stubEnv("TERM", "dumb");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(false);
 });
 
-it("return false when `TERM` is set to dumb when run on Windows", () => {
+it("return false when `TERM` is set to dumb when run on Windows", async () => {
   Object.defineProperty(process, "platform", {
     value: "win32",
   });
   vi.stubEnv("TERM", "dumb");
+  const { isColorsSupported } = await import("#supports");
 
   expect(isColorsSupported()).toBe(false);
 });
 
-it("should return true if `TERM` is set to dumb and `FORCE_COLOR` is set", () => {
+it("should return true if `TERM` is set to dumb and `FORCE_COLOR` is set", async () => {
   vi.stubEnv("TERM", "dumb");
   vi.stubEnv("FORCE_COLOR", "1");
+  const { isColorsSupported } = await import("#supports");
+
   expect(isColorsSupported()).toBe(true);
 });
