f65c3afd9895fe0e676960347ca4871f66fac8a5
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-07 15:59:28 +0200

    chore: use termenv in #supports alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     alias: {
-      "#supports": new URL("./src/supports.ts", import.meta.url).pathname,
+      "#supports": "termenv",
     },
   },
 });
