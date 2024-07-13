d37e9c3462a2331f61f0d6be50c4df3f88a2facf
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-13 08:17:44 +0200

    feat!: remove browser export

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,42 +1,20 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: [
-      "./src/index.ts",
-      "./src/supports.ts",
-    ],
-    format: ["cjs", "esm"],
-    platform: "node",
-    target: "es2022",
-    dts: true,
-    bundle: true,
-    clean: true,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
-    tsconfig: "./tsconfig.json",
+export default defineConfig({
+  entry: [
+    "./src/index.ts",
+    "./src/supports.ts",
+  ],
+  format: ["cjs", "esm"],
+  platform: "node",
+  target: "es2022",
+  dts: true,
+  bundle: true,
+  clean: true,
+  outExtension(ctx) {
+    return {
+      js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+    };
   },
-  {
-    entry: [
-      "./src/index.ts",
-      "./src/supports.browser.ts",
-    ],
-    format: ["cjs", "esm"],
-    platform: "browser",
-    outDir: "dist/browser",
-    target: "es2022",
-    dts: true,
-    treeshake: true,
-    clean: true,
-    bundle: true,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
-    tsconfig: "./tsconfig.browser.json",
-  },
-]);
+  tsconfig: "./tsconfig.json",
+});
