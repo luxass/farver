19f2f4e8fccead0617446340c448c403e9a4cabd
Lucas <lucasnrgaard@gmail.com>
2024-11-10 10:05:27 +0100

    refactor: handle newlines & nested syntax correctly (#53)
    * feat: add default export
    
    * fix: handle newlines correctly
    
    This should fix #50
    
    * fix: handle nested styles
    
    Handle nested style by replacing the end, with a start.
    
    This should fix #52
    
    * chore: update playground

--- playground.ts
+++ playground.ts
@@ -1,6 +1,52 @@
 import {
   blue,
+  cyan,
+  green,
+  inverse,
+  italic,
+  magenta,
+  red,
+  underline,
+  white,
   yellow,
+  yellowBright,
 } from "./src";
 
-console.log(`${blue("hello")}, ${yellow("world")}!`);
+console.log(`\n${inverse(" Newlines: ")} \n`);
+console.log(red.bgWhite("FIRST LINE \nSECOND LINE\nTHIRD LINE "));
+
+console.log(`\n${inverse(" Nested Syntax: ")} \n`);
+
+console.log(
+  magenta(
+    `magenta ${underline(`underline ${italic(`italic magenta`)}`)} magenta`,
+  ),
+);
+console.log(
+  green(
+    `green ${red(
+      `red ${blue(
+        `blue ${cyan(
+          `cyan ${white.italic.underline(`white italic underline`)} cyan`,
+        )} blue`,
+      )} red`,
+    )} green`,
+  ),
+);
+
+console.log(`\n${inverse(" Deep Nested Syntax: ")} \n`);
+console.log(
+  yellow(
+    `yellow ${magenta(
+      `magenta ${cyan(
+        `cyan ${blue(
+          `blue ${red(
+            `red ${green(
+              `green ${underline(`underline ${yellowBright.italic(`bright yellow italic`)} underline`)} green`,
+            )} red`,
+          )} blue`,
+        )} cyan`,
+      )} magenta`,
+    )} yellow`,
+  ),
+);
